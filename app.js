464a8b6786a6925689bd2578584812a6e27c780a
levpopov10 <82215070+levpopov10@users.noreply.github.com>
2023-12-08 21:50:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+ const isIos = () => {
+           const userAgent = window.navigator.userAgent.toLowerCase();
+           return /iphone|ipad|ipod/.test( userAgent );
+       };
+       // Проверяем, открыто ли приложение отдельно или в браузере
+       const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+
+
+       // Если приложение открыто на iOS и в браузере, то предлагаем установить
+       if (isIos() && !isInStandaloneMode()) {
+           this.setState({ isShown: true }); // На примере React
+       }
+
 var counterDisplay = document.createElement('div');
 counterDisplay.style.position = 'absolute';
 counterDisplay.style.top = '10px';
